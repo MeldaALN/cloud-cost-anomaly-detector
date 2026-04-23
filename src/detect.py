ab88a168fe3f8757811db269fd6d22cb78c5ac51
MeldaALN <meldaaslan@outlook.fr>
2026-04-23 21:55:20 +0200

    fix email security and smtp config

--- src/detect.py
+++ src/detect.py
@@ -1,13 +1,59 @@
+import os
 import csv
+import smtplib
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+
+load_dotenv()
 
 costs = []
+anomalies = []
 
-# Ouvrir le fichier et lire toutes les lignes
-with open("data/costs.csv", "r") as file:
+def send_email(anomalies):
+    smtp_server = "sandbox.smtp.mailtrap.io"
+    port = 2525
+    login = os.getenv("SMTP_LOGIN")
+    password = os.getenv("SMTP_PASSWORD")
+
+    sender = "alert@cloud.com"
+    receiver = "finops-team@test.com"
+
+    if not login or not password:
+        print("❌ Variables d'environnement SMTP manquantes.")
+        return
+
+    anomaly_details = "\n".join(
+        [f"- {row['date']} | {row['service']} | {row['cost']}€" for row in anomalies]
+    )
+
+    body = f"""🚨 Cloud Cost Alert
+
+Des coûts anormaux ont été détectés :
+
+{anomaly_details}
+"""
+
+    message = MIMEText(body)
+    message["Subject"] = "Cloud Cost Alert"
+    message["From"] = sender
+    message["To"] = receiver
+
+    try:
+        server = smtplib.SMTP(smtp_server, port, timeout=10)
+        server.starttls()
+        server.login(login, password)
+        server.sendmail(sender, [receiver], message.as_string())
+        server.quit()
+        print("📧 Email envoyé avec succès via Mailtrap !")
+    except Exception as e:
+        print("❌ Erreur lors de l'envoi de l'email :", repr(e))
+
+# Lire les données
+with open("data/costs.csv", "r", encoding="utf-8") as file:
     reader = csv.DictReader(file)
     rows = list(reader)
 
-# Récupérer tous les coûts
+# Récupérer les coûts
 for row in rows:
     cost = float(row["cost"])
     costs.append(cost)
@@ -29,4 +75,12 @@ for row in rows:
     cost = float(row["cost"])
 
     if cost > average * 2:
+        anomalies.append(row)
         print(f"🚨 Anomalie détectée le {row['date']} pour {row['service']} ({cost}€)")
+
+print("")
+
+if anomalies:
+    send_email(anomalies)
+else:
+    print("✅ Aucun coût anormal détecté. Aucun email envoyé.")
